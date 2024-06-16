e8e06d62b637b1ed52854b61e97defd70e3feda7
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-16 22:58:31 +0530

    Create Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { db } from '../firebase';
+import { ref, set, get, update } from 'firebase/database';
+
+const Home = () => {
+
+    const [data, setData] = useState();
+
+    const writeUserData = (e) => {
+
+        e.preventDefault();
+
+        const dbRef = ref(db, `users/${data}`);
+        set(dbRef, {
+            name: data,
+            age: 31,
+            isStudent: false
+        }).then(() => {
+            console.log('Data saved successfully!');
+        })
+            .catch((error) => {
+                console.log('Failed to save data: ', error);
+            });;
+    }
+
+
+
+
+
+
+    return (
+
+        <div className='p-8 flex xsm:bg-black sm:bg-green-400 md:bg-slate-900 lg:bg-red-500 xl:bg-amber-400 2xl:bg-sky-500     '>
+
+
+
+            <form onSubmit={writeUserData}>
+                <input type="text" onChange={(e) => setData(e.target.value)} />
+                <input type="submit" className=' bg-red-600' />
+            </form>
+
+
+
+        </div>
+    )
+}
+
+export default Home
