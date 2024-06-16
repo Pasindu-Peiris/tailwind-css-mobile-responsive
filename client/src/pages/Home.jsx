9def0a6a9e1a9a88c44463c3bfe56c773bd1180b
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-16 22:58:59 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import { db } from '../firebase';
 import { ref, set, get, update } from 'firebase/database';
 
@@ -9,7 +9,7 @@ const Home = () => {
     const writeUserData = (e) => {
 
         e.preventDefault();
-
+    
         const dbRef = ref(db, `users/${data}`);
         set(dbRef, {
             name: data,
@@ -17,10 +17,10 @@ const Home = () => {
             isStudent: false
         }).then(() => {
             console.log('Data saved successfully!');
-        })
-            .catch((error) => {
-                console.log('Failed to save data: ', error);
-            });;
+          })
+          .catch((error) => {
+            console.log('Failed to save data: ', error);
+          });;
     }
 
 
@@ -28,21 +28,24 @@ const Home = () => {
 
 
 
-    return (
+  return (
 
-        <div className='p-8 flex xsm:bg-black sm:bg-green-400 md:bg-slate-900 lg:bg-red-500 xl:bg-amber-400 2xl:bg-sky-500     '>
+    <div className='p-8 flex xsm:bg-black sm:bg-green-400 md:bg-slate-900 lg:bg-red-500 xl:bg-amber-400 2xl:bg-sky-500     '>
+
+        <div className=' bg-green-500 w-[200px] h-20'>div 1</div>
+        <div className=' bg-green-500 w-[200px] h-20'>div 2</div>
+        <div className=' bg-green-500 w-[200px] h-20'>div 3</div>
+        <div className=' bg-green-500 w-[200px] h-20'>div 4</div>
+
+<form onSubmit={writeUserData}>
+    <input type="text" onChange={(e) => setData(e.target.value)} />
+    <input type="submit" className=' bg-red-600'/>
+</form>
 
 
 
-            <form onSubmit={writeUserData}>
-                <input type="text" onChange={(e) => setData(e.target.value)} />
-                <input type="submit" className=' bg-red-600' />
-            </form>
-
-
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Home
