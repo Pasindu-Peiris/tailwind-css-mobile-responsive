b8207d7d3624b37b2bc256c31f4e89da43648b27
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-20 00:24:50 +0530

    Create Fotter.jsx

---
+++ client/src/pages/Fotter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fotter = () => {
+  return (
+    <div className=' bg-slate-950 text-white w-[100%] h-[8vh] text-center items-center flex justify-center'> Copyright © 2024 - Pasindu Peiris</div>
+  )
+}
+
+export default Fotter
