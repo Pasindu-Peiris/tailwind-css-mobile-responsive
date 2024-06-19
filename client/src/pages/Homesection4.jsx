34de8cf21d548836d3c9769a38ffb357d24d71dc
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-19 23:48:26 +0530

    Update Homesection4.jsx

--- client/src/pages/Homesection4.jsx
+++ client/src/pages/Homesection4.jsx
@@ -1,28 +1,96 @@
 import React from 'react'
 
 const Homesection4 = () => {
-  return (
-    <div className=' w-[100%] h-[100vh] xl:mt-5'>
+    return (
+        <div className=' w-[100%] h-[100vh] xl:mt-5 '>
 
-        <div className="text-content grid justify-center items-center text-center">
+            <div className="text-content grid justify-center items-center text-center">
 
-            <p className=' font-semibold text-[#00646b]'>SERVICES</p>
-            <h1 className=' font-bold text-4xl'>What's Intresting about dog's</h1>
-        </div>
-
-        <div className="service-block ">
-
-            <div className="service-boxes">
-                
+                <p className=' font-semibold text-[#00646b]'>SERVICES</p>
+                <h1 className=' font-bold text-4xl'>What's Intresting about dog's</h1>
             </div>
 
+            <div className="w-[100%]  service-block grid xl:grid-cols-3 items-center justify-center gap-10 p-20 md:grid-cols-2 ">
+
+                <div className="service-boxes">
+                    <div className="icon">
+                    <i class="fas text-[#00646b] text-2xl fa-paw"></i>
+                    </div>
+                    <div className="text-p">
+                        <p className='font-semibold my-2'>Jealousy in the air!</p>
+                    </div>
+                    <div className="sec-p">
+                        <p>At different stage of your life, you have a unique set of concerns, & we understand you. We provide professional advice.</p>
+                    </div>
+                </div>
+
+                <div className="service-boxes">
+                    <div className="icon">
+                    <i class="fas text-[#00646b] text-2xl fa-home"></i>
+                    </div>
+                    <div className="text-p">
+                        <p className='font-semibold my-2'>Running in circles</p>
+                    </div>
+                    <div className="sec-p">
+                        <p>At different stage of your life, you have a unique set of concerns, & we understand you. We provide professional advice.</p>
+                    </div>
+                </div>
+
+                <div className="service-boxes">
+                    <div className="icon">
+                    <i class="fas text-[#00646b] text-2xl fa-dog"></i>
+                    </div>
+                    <div className="text-p">
+                        <p className='font-semibold my-2'>The most popular breed in the US</p>
+                    </div>
+                    <div className="sec-p">
+                        <p>At different stage of your life, you have a unique set of concerns, & we understand you. We provide professional advice.</p>
+                    </div>
+                </div>
+
+                <div className="service-boxes">
+                    <div className="icon">
+                    <i class="fas text-[#00646b] text-2xl fa-medal"></i>
+                    </div>
+                    <div className="text-p">
+                        <p className='font-semibold my-2'>Superdog</p>
+                    </div>
+                    <div className="sec-p">
+                        <p>At different stage of your life, you have a unique set of concerns, & we understand you. We provide professional advice.</p>
+                    </div>
+                </div>
+
+                <div className="service-boxes">
+                    <div className="icon">
+                    <i class="fas text-[#00646b] text-2xl fa-trophy"></i>
+                    </div>
+                    <div className="text-p">
+                        <p className='font-semibold my-2'>The Oldest dog ever</p>
+                    </div>
+                    <div className="sec-p">
+                        <p>At different stage of your life, you have a unique set of concerns, & we understand you. We provide professional advice.</p>
+                    </div>
+                </div>
+
+                <div className="service-boxes">
+                    <div className="icon">
+                    <i class="fas text-[#00646b] text-2xl fa-heartbeat"></i>
+                    </div>
+                    <div className="text-p">
+                        <p className='font-semibold my-2'>Lower your blood pressure</p>
+                    </div>
+                    <div className="sec-p">
+                        <p>At different stage of your life, you have a unique set of concerns, & we understand you. We provide professional advice.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
         </div>
-
-
-
-
-    </div>
-  )
+    )
 }
 
 export default Homesection4
