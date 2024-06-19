3647395100b31aa6644b22996b8c96a40c5df124
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-19 20:44:10 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import Homesection1 from './pages/Homesection1.jsx';
+import Homesection2 from './pages/Homesection2.jsx';
+import Homesection3 from './pages/Homesection3.jsx';
 
 function App() {
   return (
     <div className="App">
       <Homesection1/>
+      <Homesection2/>
+      <Homesection3/>
     
     </div>
   );
