f6b3e73ec01c19fd1763534a1886381a78065471
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-19 20:47:06 +0530

    Update Homesection3.jsx

--- client/src/pages/Homesection3.jsx
+++ client/src/pages/Homesection3.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 
 const Homesection3 = () => {
-  return (
-    <div className=' w-[100%] h-[70vh]'>
+    return (
+        <div className=' w-[100%] h-[70vh]'>
 
-        <div className="text-content">
+            <div className="text-content">
 
-            <div className="sm-text">
-            <p>ABOUT US</p>
+                <div className="sm-text">
+                    <p>ABOUT US</p>
+                </div>
+                <div className="text-h1">
+                    <h1 className='text-4xl font-bold'>The dogs lives for the day,the hour,even the moment.</h1>
+                </div>
+
+                <div className="sm-p">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi qui atque nam molestias odit ipsa tempore, liber.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio doloribus tenetur ducimus numquam voluptates minus molestiae assumenda.</p>
+                </div>
+
+                <div className="menu mt-4">
+                    <a href='#!' className=' px-6 py-3 bg-[#e6eeeb] text-black rounded-sm'>Services <i class="fa-solid fa-chevron-right mx-2 font-bold"></i></a>
+                    <a href='#!' className=' px-6 py-3 mx-5 bg-[#00646b] text-white rounded-sm'>Contact <i class="fa-solid fa-chevron-right mx-2 font-bold"></i></a>
+                </div>
+
+
+
+
+
             </div>
-            <div className="text-h1">
-                
+
+            <div className="image-content">
+
             </div>
 
 
         </div>
-
-        <div className="image-content">
-
-        </div>
-
-
-    </div>
-  )
+    )
 }
 
 export default Homesection3
