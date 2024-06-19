652979ee9fc1b7f7e869acd032d588b2e6f7e8a7
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-19 20:44:03 +0530

    Create Homesection3.jsx

---
+++ client/src/pages/Homesection3.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Homesection3 = () => {
+  return (
+    <div className=' w-[100%] h-[70vh]'>
+
+        <div className="text-content">
+
+            <div className="sm-text">
+            <p>ABOUT US</p>
+            </div>
+            <div className="text-h1">
+                
+            </div>
+
+
+        </div>
+
+        <div className="image-content">
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Homesection3
