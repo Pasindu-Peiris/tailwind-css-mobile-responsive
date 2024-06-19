ff78ecc744e791f7a25bcbf1f0514fa54d524536
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-19 23:04:57 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Homesection1 from './pages/Homesection1.jsx';
 import Homesection2 from './pages/Homesection2.jsx';
 import Homesection3 from './pages/Homesection3.jsx';
+import Homesection4 from './pages/Homesection4.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Homesection1/>
       <Homesection2/>
       <Homesection3/>
+      <Homesection4/>
     
     </div>
   );
