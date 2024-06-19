765722254680e775b81c57936ba8f0f87b895c25
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-19 23:05:04 +0530

    Update Homesection2.jsx

--- client/src/pages/Homesection2.jsx
+++ client/src/pages/Homesection2.jsx
@@ -13,13 +13,13 @@ const Homesection2 = () => {
 
         <div className="img-block xl:flex xl:flex-row w-[100%] min-h-min-[20vh] mt-6 md:px-12 justify-center items-center gap-20 flex flex-wrap md:h-auto">
 
-            <img src={Img1} alt="" width={60} className='grayscale' />
-            <img src={Img2} alt="" width={60} className='grayscale' />
-            <img src={Img3} alt="" width={60} className='grayscale' />
-            <img src={Img4} alt="" width={60} className='grayscale' />
-            <img src={Img5} alt="" width={60} className='grayscale' />
-            <img src={Img6} alt="" width={60} className='grayscale' />
-            <img src={Img7} alt="" width={60} className='grayscale' />
+            <img src={Img1} alt="" width={60} className='grayscale hover:grayscale-0' />
+            <img src={Img2} alt="" width={60} className='grayscale hover:grayscale-0' />
+            <img src={Img3} alt="" width={60} className='grayscale hover:grayscale-0' />
+            <img src={Img4} alt="" width={60} className='grayscale hover:grayscale-0' />
+            <img src={Img5} alt="" width={60} className='grayscale hover:grayscale-0' />
+            <img src={Img6} alt="" width={60} className='grayscale hover:grayscale-0' />
+            <img src={Img7} alt="" width={60} className='grayscale hover:grayscale-0' />
 
         </div>
 
