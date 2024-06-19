49c5341893b9821d5f8593290338f2757dc35034
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-19 23:05:01 +0530

    Create Homesection4.jsx

---
+++ client/src/pages/Homesection4.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Homesection4 = () => {
+  return (
+    <div className=' w-[100%] h-[100vh] xl:mt-5'>
+
+        <div className="text-content grid justify-center items-center text-center">
+
+            <p className=' font-semibold text-[#00646b]'>SERVICES</p>
+            <h1 className=' font-bold text-4xl'>What's Intresting about dog's</h1>
+        </div>
+
+        <div className="service-block ">
+
+            <div className="service-boxes">
+                
+            </div>
+
+        </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Homesection4
