db61cb687b5534574f4488b1362c710735c707f9
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-22 23:50:11 +0530

    Update Homesection4.jsx

--- client/src/pages/Homesection4.jsx
+++ client/src/pages/Homesection4.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Homesection4 = () => {
     return (
-        <div className=' w-[100%] h-[80vh] md:min-h-[100vh] md:py-12 xl:mt-5 grid flex-col justify-center items-center'>
+        <div className=' w-[100%] h-[80vh] md:min-h-[100vh] sm:w-[auto] xsm:w-[auto] md:py-12 sm:mt-12 xsm:mt-12 xl:mt-5 grid flex-col justify-center items-center'>
 
-            <div className="text-content grid justify-center items-center text-center">
+            <div className="text-content grid justify-center items-center text-center ">
 
                 <p className=' font-semibold text-[#00646b]'>SERVICES</p>
-                <h1 className=' font-bold text-4xl'>What's Intresting about dog's</h1>
+                <h1 className=' font-bold xl:text-4xl md:text-4xl sm:text-3xl xsm:text-3xl'>What's Intresting about dog's</h1>
             </div>
 
-            <div className="w-[100%]  service-block grid xl:grid-cols-3 items-center justify-center xl:gap-10 md:gap-5 md:p-10 xl:p-20 md:grid-cols-2 ">
+            <div className="w-[100%] sm:p-5 xsm:p-4 xsm:gap-6 sm:gap-5 sm:mt-10 xsm:mt-12 service-block grid xl:grid-cols-3 items-center justify-center xl:gap-10 md:gap-5 md:p-10 xl:p-20 md:grid-cols-2 ">
 
                 <div className="service-boxes">
                     <div className="icon">
