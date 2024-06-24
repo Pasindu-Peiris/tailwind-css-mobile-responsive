49278582b9642cb37cce3c1a8bc4c8780e453843
Pasindu Peiris <it21831454@my.sliit.lk>
2024-06-25 00:25:41 +0530

    Update Homesection6.jsx

--- client/src/pages/Homesection6.jsx
+++ client/src/pages/Homesection6.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Homesection6 = () => {
   return (
-    <div className=' w-[100%] h-min-[40vh] md:mb-5 xl:flex md:grid xl:flex-row  md:flex-col md:gap-4 justify-center items-center '>
+    <div className=' w-[100%] h-[40vh] md:h-[auto] sm:h-[auto] sm:mb-10 sm:mt-12 xsm:bg-green-500 md:mb-5 xsm:mt-12 xl:flex md:grid xl:flex-row  md:flex-col sm:grid sm:flex-col md:gap-4 sm:gap-4 justify-center items-center '>
 
-        <div className="box-1 p-5 border-2 border-gray-200 w-[400px] h-[auto] xl:mx-5 ">
+        <div className="box-1 p-5 border-2 border-gray-200 w-[400px] sm:w-[100%] xsm:w-[100%] h-[auto] xl:mx-5 ">
             <p className=' font-semibold my-2 text-xl'>Address</p>
             <p>246 Alla ,Lake Lane,No 24268,Sri Lanka</p>
             <p>Phone: <span className=' text-[#00646b]'>+94 779974562</span></p>
@@ -12,7 +12,7 @@ const Homesection6 = () => {
 
         </div>
 
-        <div className="box-1 p-5 border-2 border-gray-200 w-[400px] h-[auto] xl:mx-5">
+        <div className="box-1 p-5 border-2 border-gray-200 w-[400px] h-[auto] xl:mx-5 sm:w-[100%] xsm:w-[100%]">
             <p className=' font-semibold my-2 text-xl' >Available</p>
             <p>MON - WED  <span className=' text-[#00646b]'>(09:00 AM - 08:00 PM)</span></p>
             <p>THU - FIR  <span className=' text-[#00646b]'>(09:00 AM - 08:00 PM)</span></p>
